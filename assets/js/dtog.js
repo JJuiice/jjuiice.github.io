a1868e998d9833ee70babb8eb1f2ee36376347a8
Ojas Anand <oanand@fastmail.com>
2022-03-07 05:36:51 +0000

    Moved DTS generation to within javascript

--- assets/js/dtog.js
+++ assets/js/dtog.js
@@ -1,37 +1,58 @@
-const p8 = document.querySelector("#P8");
-const p9 = document.querySelector("#P9");
+// BB DTS Constants
+const header = `
+/dts-v1/;
+/plugin/;\n`.trimStart();
+const am355x_bbw_bbb_bash = "#include &lt;dt-bindings/board/am335x-bbw-bbb-bash.h&gt;";
+const bb_dts_comm_init = `
+/ {
+  compatible = "ti,beaglebone", "ti,beaglebone-black";
+  part-number = "BB-<your-part-name>";
+  version = "00A0";\n`;
+const exc_use = `
+  exclusive-use=`.trimStart();
+const frag_0 = `
+  /*
+   * Helper to show loaded overlays under /proc/device-tree/overlays/
+   */
+  fragment@0 {
+    target-path="/";
+    __overlay__ {
+      chosen {
+        overlays {
+          BB-<your-part-name>-00A0 = __TIMESTAMP__;
+        };
+      };
+    };
+  };\n`;
+const frag_1 = `
+  /*
+   * Disable pins to be configured so that conflicts may be avoided
+   */
+  fragment@1 {
+    target = <&ocp>;
+    __overlay__ {
+    };
+  };\n`;
+const am33xx_pinmux_conf = `
+  /*
+   * AM33XX PINMUX Configuration
+   */
+  fragment@2 {
+    target = &lt;&am33xx_pinmux&gt;;
+    __overlay__ {
+    };
+  };\n`;
+const bone_pinmux_helper_conf = `
+  /*
+   * Use bone-pinmux-helper to apply our pinmux
+   */
+  fragment@3 {
+    target = &lt;&ocp&gt;;
+    __overlay__ {
+    };
+  };\n`;
+const close_brac = "};"
 
-const pin_selects = document.querySelector("#pin-selects");
-const pin_select_p8 = document.querySelector("#pin-select-p8");
-const pin_select_p9 = document.querySelector("#pin-select-p9");
-
-const mode_selects = document.querySelector("#mode-selects");
-
-function showSelect(select_val) {
-	const selects = document.querySelectorAll("#mode-selects select");
-	for (var i = 0; i < selects.length; i++)
-		selects[i].style.display = "none";
-	document.querySelector("#" + select_val).style.display = "block";
-}
-
-p8.onclick = function () {
-	pin_selects.className = "p8";
-	mode_selects.className = "p8";
-	showSelect(pin_select_p8.value);
-};
-
-p9.onclick = function () {
-	pin_selects.className = "p9";
-	mode_selects.className = "p9";
-	showSelect(pin_select_p9.value);
-};
-
-pin_select_p8.onchange = function () {
-	mode_selects.className = "p8";
-	showSelect(pin_select_p8.value);
-};
-
-pin_select_p9.onchange = function () {
-	mode_selects.className = "p9";
-	showSelect(pin_select_p9.value);
+function generateDTS() {
+	document.querySelector("#dts_out").textContent = header + bb_dts_comm_init + frag_0 + frag_1 + close_brac;
 }
